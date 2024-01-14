be63118cd4258340d5c9a1836b1a6510d6263a3d
ARSHAD V P <Arshad1071@users.noreply.github.com>
2024-01-14 13:14:50 +0530

    save changes

---
+++ src/components/createTaskForm/createTaskFrom.tsx
@@ -0,0 +1,11 @@
+import React, { FC, ReactElement } from 'react';
+import PropTypes from 'prop-types';
+import { Box } from '@mui/material';
+
+const createTaskFrom: FC = (): ReactElement => {
+  return <Box></Box>;
+};
+
+createTaskFrom.propTypes = {};
+
+export default createTaskFrom;
