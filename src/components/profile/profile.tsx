57753102ee72dc1e7e63d9973154542661434d28
ARSHAD V P <Arshad1071@users.noreply.github.com>
2024-01-14 10:55:36 +0530

    first commit

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,45 @@
+import React, { FC, ReactElement } from 'react';
+import PropTypes from 'prop-types';
+import { Avatar, Box, Typography } from '@mui/material';
+
+interface Iprofile {
+  name?: string;
+}
+
+const profile: FC = ({
+  name = 'Arshad',
+}: any): ReactElement => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar
+        sx={{
+          width: '96px',
+          height: '96px',
+          backgroundColor: 'primary.main',
+          marginBottom: '16px',
+        }}
+      >
+        <Typography variant="h4" color="text.primary">
+          {`${name.substring(0, 1)}`}
+        </Typography>
+      </Avatar>
+      <Typography variant="h6">
+        Welcome, {`${name}`}
+      </Typography>
+      <Typography variant="subtitle1">
+        This is your prsonal task manager{' '}
+      </Typography>
+    </Box>
+  );
+};
+
+profile.propTypes = { name: PropTypes.string };
+
+export default profile;
